b4618c3aec44bb4d20978c9a070bfdf6ae3ad7ae
iliya <iliya2012hajjar@gmail.com>
2023-05-20 03:38:58 +0330

    sales: api added
    updated: Dockerfile
    updated: docker-compose.yml
    add: requirements.txt
    add: api added sales.py
    revise: schema.py

--- sales/schema.py
+++ sales/schema.py
@@ -1,5 +1,3 @@
-import uuid
-from utils.uid import UUID
 from sqlalchemy.sql import func
 import sqlalchemy.types as types
 from dataclasses import dataclass
@@ -21,10 +19,13 @@ class Order(Base):
 
     __tablename__ = 'order_table'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    transaction_id = Column(Integer)
-    user_id = Column(Integer)
+    transaction_id = Column(Integer, nullable=True)
+    user_id = Column(Integer, nullable=False)
     created_at = Column(DateTime(timezone=False), server_default=func.now())
     updated_at = Column(types.TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
+
+    def to_dict(self):
+        return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
 
 
 @dataclass
@@ -32,11 +33,16 @@ class OrderDetails(Base):
     id: int
     order_id: str
     product_id: str
+    product_count: int
     created_at: str
 
     __tablename__ = 'order_details_table'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    product_id = Column(Integer)
-    order_id = Column(Integer, ForeignKey(Order.id))
+    product_count = Column(Integer, nullable=False)
+    product_id = Column(Integer, nullable=False)
+    order_id = Column(Integer, ForeignKey(Order.id), nullable=False)
     order = relationship('Order', foreign_keys='OrderDetails.order_id')
     created_at = Column(DateTime(timezone=False), server_default=func.now())
+
+    def to_dict(self):
+        return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
