3f4de4a8c2c4d37dd14f1e1302b6037fd704ff6d
iliya <iliya2012hajjar@gmail.com>
2023-06-10 02:13:15 +0330

    warehouse: add new api routes

--- api_gateway/gateway.py
+++ api_gateway/gateway.py
@@ -35,6 +35,19 @@ def signup():
         return {"status": 400}
 
 
+@server.route('/categories', methods=['GET'], strict_slashes=False)
+def get_categories():
+    access, err = validate.token(request)
+
+    if err:
+        return err
+    try:
+        response = requests.request("GET", f"http://warehouse:5000/api/v1/categories").json()
+        return jsonify(response)
+    except Exception as e:
+        abort(400, 'Can not add new category !')
+
+
 @server.route('/category/<int:id>', methods=['GET'], strict_slashes=False)
 def get_category(id):
     access, err = validate.token(request)
@@ -45,7 +58,7 @@ def get_category(id):
         response = requests.request("GET", f"http://warehouse:5000/api/v1/category/{id}").json()
         return jsonify(response)
     except Exception as e:
-        abort(400, 'Can not add new category !')
+        abort(400, 'Can not get category !')
 
 
 @server.route('/category', methods=['POST'], strict_slashes=False)
@@ -74,7 +87,7 @@ def get_suppliers():
         response = requests.request("GET", f"http://warehouse:5000/api/v1/suppliers").json()
         return jsonify(response)
     except Exception as e:
-        abort(400, 'Can not add new category !')
+        abort(400, 'Can not get suppliers !')
 
 
 @server.route('/supplier/<int:id>', methods=['GET'], strict_slashes=False)
@@ -87,7 +100,7 @@ def get_supplier(id):
         response = requests.request("GET", f"http://warehouse:5000/api/v1/supplier/{id}").json()
         return jsonify(response)
     except Exception as e:
-        abort(400, 'Can not add new category !')
+        abort(400, 'Can not get supplier !')
 
 
 @server.route('/supplier', methods=['POST'], strict_slashes=False)
@@ -103,7 +116,7 @@ def set_supplier():
                                     data=json.dumps(data)).json()
         return response
     except Exception as e:
-        abort(400, 'Can not add new category !')
+        abort(400, 'Can not add new supplier !')
 
 
 @server.route('/products', methods=['GET'], strict_slashes=False)
@@ -116,7 +129,7 @@ def get_products():
         response = requests.request("GET", f"http://warehouse:5000/api/v1/products").json()
         return jsonify(response)
     except Exception as e:
-        abort(400, 'Can not get new products !')
+        abort(400, 'Can not get products !')
 
 
 @server.route('/product/<int:id>', methods=['GET'], strict_slashes=False)
@@ -129,7 +142,7 @@ def get_product(id):
         response = requests.request("GET", f"http://warehouse:5000/api/v1/product/{id}").json()
         return jsonify(response)
     except Exception as e:
-        abort(400, 'Can not add new product !')
+        abort(400, 'Can not get product !')
 
 
 @server.route('/product', methods=['POST'], strict_slashes=False)
@@ -145,7 +158,23 @@ def set_product():
                                     data=json.dumps(data)).json()
         return response
     except Exception as e:
-        abort(400, 'Can not add new category !')
+        abort(400, 'Can not add new product !')
+
+
+@server.route('/product/<int:product_id>', methods=['PUT'], strict_slashes=False)
+def update_product(product_id):
+    access, err = validate.token(request)
+
+    if err:
+        return err
+    try:
+        data = request.get_json()
+        response = requests.request("PUT", f"http://warehouse:5000/api/v1/product/{product_id}",
+                                    headers={'Content-Type': 'application/json'},
+                                    data=json.dumps(data)).json()
+        return response
+    except Exception:
+        abort(400, 'Can not update product !')
 
 
 @server.route('/order/<int:id>', methods=['GET'], strict_slashes=False)
@@ -158,7 +187,7 @@ def get_order(id):
         response = requests.request("GET", f"http://sales:5000/api/v1/order_detail/{id}").json()
         return jsonify(response)
     except Exception as e:
-        abort(400, 'Can not add new category !')
+        abort(400, 'Can not get order !')
 
 
 @server.route('/orders', methods=['GET'], strict_slashes=False)
@@ -171,7 +200,7 @@ def get_orders():
         response = requests.request("GET", f"http://sales:5000/api/v1/order_details").json()
         return jsonify(response)
     except Exception as e:
-        abort(400, 'Can not add new category !')
+        abort(400, 'Can not get order !')
 
 
 @server.route('/order', methods=['POST'], strict_slashes=False)
@@ -187,7 +216,7 @@ def set_order():
                                     data=json.dumps(data)).json()
         return response
     except Exception:
-        abort(400, 'Can not add new category !')
+        abort(400, 'Can not add new order !')
 
 
 @server.route('/payment', methods=['POST'], strict_slashes=False)
@@ -203,7 +232,7 @@ def set_payment():
                                     data=json.dumps(data)).json()
         return response
     except Exception:
-        abort(400, 'Can not add new category !')
+        abort(400, 'Can not add new payment !')
 
 
 @server.route('/payment', methods=['GET'], strict_slashes=False)
@@ -216,7 +245,7 @@ def get_payment():
         response = requests.request("GET", f"http://accounting:8000/api/v1/transaction/payments").json()
         return jsonify(response)
     except Exception as e:
-        abort(400, 'Can not add new category !')
+        abort(400, 'Can not get payment !')
 
 
 @server.route('/transaction/<string:tx_id>', methods=['PUT'], strict_slashes=False)
@@ -232,7 +261,7 @@ def update_transaction(tx_id):
                                     data=json.dumps(data)).json()
         return response
     except Exception:
-        abort(400, 'Can not add new category !')
+        abort(400, 'Can not update transaction !')
 
 
 @server.route('/transactions', methods=['GET'], strict_slashes=False)
@@ -245,7 +274,7 @@ def get_transactions():
         response = requests.request("GET", f"http://accounting:8000/api/v1/transaction/transactions").json()
         return jsonify(response)
     except Exception as e:
-        abort(400, 'Can not add new category !')
+        abort(400, 'Can not get transaction !')
 
 
 if __name__ == "__main__":
